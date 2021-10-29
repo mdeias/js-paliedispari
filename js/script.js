a0234d9f7ba9e77ea8b720dd9b94048aa07ceb2d
mdeias <mdeias@icloud.com>
2021-10-29 17:11:42 +0200

    ragionamento con commenti

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// **Palidroma**
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo una parola all'utente 
+// creo variabile che di default non è polindroma
+// creo una condizione che le confronta
+// creo o cerco una funzione da applicare come condizione 
+
+
+
+
+
+
+
+// **Pari e Dispari**
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// chiedo all'utente pari o dispari 
+// chiedo all'utente un numero da 1 a 5
+// creo funzione che ci restituisca un numero casuale da 1 a 5
+// sommiamo i due numeri e decretiamo il vincitore con una condizione
